63f7984a0c3ea5039df1fd7474bba7fe94051b91
SotaNoniwa <surfersou@gmail.com>
2023-10-08 13:08:49 +0200

    create sample page for producing artistic image

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home.js";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
+import Art from "./components/Art.js";
 
 const App = () => {
     return (
@@ -10,6 +11,7 @@ const App = () => {
             <Header />
             <Routes>
                 <Route exact path="/" element={<Home />} />
+                <Route exact path="/art" element={<Art />} />
             </Routes>
             <Footer />
         </div>
--- client/src/components/Art.js
+++ client/src/components/Art.js
@@ -1,11 +1,9 @@
 import { React, useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
-function Home() {
+function Art() {
 
     const [file, setFile] = useState();
-    const [prediction, setPrediction] = useState("");
 
     const createObjectURL = (file) => {
         if (window.URL && window.URL.createObjectURL) {
@@ -33,22 +31,14 @@ function Home() {
             formData.append("file", event.target.elements.file.files[0]);
 
             // send POST request to API server with file data
-            const res = await axios.post("http://127.0.0.1:5000/predict", formData, {
+            const res = await axios.post("http://127.0.0.1:5000/sample", formData, {
                 headers: {
                     "Content-Type": "multipart/form-data",
                 },
             });
-
-            // update prediction to display it on screen
-            setPrediction(res.data);
         } catch (err) {
             console.log(err);
         }
-    }
-
-    let navigate = useNavigate();
-    function routeChange() {
-        navigate("/art");
     }
 
     return (
@@ -56,29 +46,21 @@ function Home() {
             <section id="main">
                 <div id="app">
                     <div>
-                        <h3>Select an image to classify!</h3>
+                        <h3>Select an image and create an Art!</h3>
                     </div>
                     <form id="imageForm" encType="multipart/form-data" method="POST" onSubmit={onSubmit}>
                         <label htmlFor="imageFile">Choose File</label>
                         <input id="imageFile" name="file" type="file" onChange={handleChange} />
-                        {file && (
-                            <>
-                                <img src={file} alt="selected" />
-                                <label htmlFor="classifyImage">Classify Image</label>
-                                <input id="classifyImage" type="submit" />
-                            </>
-                        )}
-                        {prediction !== "" &&
-                            <div className="prediction">{prediction}</div>}
+                        {file && <img src={file} alt="selected" />}
+                        <h3 style={{ marginTop: "50px" }}>Choose Art style!</h3>
+                        <p style={{ marginBottom: "50px" }}>Option1, Option2, Option3...</p>
+                        <label htmlFor="createArt">Create Art!</label>
+                        <input id="createArt" type="submit" />
                     </form>
                 </div>
             </section>
-            <div className="navigate">
-                <label htmlFor="navigateArt">change route</label>
-                <button id="navigateArt" onClick={routeChange}></button>
-            </div>
         </div>
     );
 }
 
-export default Home;
+export default Art;
